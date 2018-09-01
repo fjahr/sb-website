6a0af6b949ffb41f2584ff1dac69ffc38e9d470a
Fabian Jahr <fabian.jahr@whu.edu>
2018-09-01 14:17:18 +0200

    initial commit

---
+++ js/sb.js
@@ -0,0 +1,16 @@
+var $form = $('form#email-form'),
+    url = 'https://script.google.com/macros/s/AKfycbwrFY6XZESSXwwwAozKaUuUf44GBI-e5gSQWCTq0UV5ZBt4L1VB/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serialize()
+  }).done(function() {
+    $("input#email-input").val("");
+    $("#email-success").show();
+  }
+  );
+})
